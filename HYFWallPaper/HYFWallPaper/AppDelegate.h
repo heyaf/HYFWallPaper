0708bc1c95fbc3068c0803b878b8596627ee449c
熊本熊 <apple@hongdingdeMac-mini.local>
2017-11-08 16:41:46 +0800

    first commit

---
+++ HYFWallPaper/HYFWallPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYFWallPaper
+//
+//  Created by 弘鼎 on 2017/11/8.
+//  Copyright © 2017年 贺亚飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
